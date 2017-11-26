1635ddac3a2ad42ec05cafa5eef067f48d7c549c
hkg25 <harrygenius099@gmail.com>
2017-11-26 12:34:28 +0530

    update script

--- playground/mongo-update.js
+++ playground/mongo-update.js
@@ -6,6 +6,29 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp",(err,db)=>{
     }
     console.log("Connected to MongoDB server");
 
+    // find one and update
+    // db.collection("Todos").findOneAndUpdate(
+    //     {_id:new ObjectID("5a1a64eb46dbac965cf5540b")},
+    //     { $set :{completed:true} },
+    //     {
+    //         returnOriginal : false
+    //     }
+    // ).then(
+    //     (result)=>console.log(JSON.stringify(result,undefined,3)),
+    //     (err)=>console.log(err)
+    // );
+
+    db.collection("Users").findOneAndUpdate(
+        {_id:new ObjectID("5a1a5f5346dbac965cf55254")},
+        { $set :{name : "Harish Garg"}, $inc :{age : 5} },
+        {
+            returnOriginal : false
+        }
+    ).then(
+        (result)=>console.log(JSON.stringify(result,undefined,3)),
+        (err)=>console.log(err)
+    );
+
     //db.close(); 
 
 });
